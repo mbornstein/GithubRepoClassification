5ffce9c3de1ea4cce709a59c57225a328869579a
mbornstein <marvin.bornstein@student.hpi.uni-potsdam.de>
2016-10-27 09:11:41 +0200

    add data

---
+++ data/crawl_raw.sh
@@ -0,0 +1,43 @@
+touch raw_api_repo_detail.txt
+
+# Examples for DEV-Repositories
+curl https://api.github.com/repos/briantemple/homeworkr >> raw_api_repo_detail.txt
+curl https://api.github.com/repos/spring-projects/spring-boot >> raw_api_repo_detail.txt
+curl https://api.github.com/repos/facebook/react >> raw_api_repo_detail.txt
+curl https://api.github.com/repos/nodegit/nodegit >> raw_api_repo_detail.txt
+curl https://api.github.com/repos/scipy/scipy >> raw_api_repo_detail.txt
+
+# Examples for HW-Repositories
+curl https://api.github.com/repos/spez/RottenTomatoes >> raw_api_repo_detail.txt
+curl https://api.github.com/repos/m2mtech/calculator-2015 >> raw_api_repo_detail.txt
+curl https://api.github.com/repos/bcaffo/751and2 >> raw_api_repo_detail.txt
+curl https://api.github.com/repos/HPI-SWA-Teaching/SWT16-Project-08 >> raw_api_repo_detail.txt
+curl https://api.github.com/repos/uwhpsc-2016/example-python-homework >> raw_api_repo_detail.txt
+
+# Examples for EDU-Repositories
+curl https://api.github.com/repos/DataScienceSpecialization/courses >> raw_api_repo_detail.txt
+curl https://api.github.com/repos/githubteacher/intro-november-2015 >> raw_api_repo_detail.txt
+curl https://api.github.com/repos/alex/what-happens-when >> raw_api_repo_detail.txt
+curl https://api.github.com/repos/MostlyAdequate/mostly-adequate-guide >> raw_api_repo_detail.txt
+curl https://api.github.com/repos/AllThingsSmitty/jquery-tips-everyone-should-know >> raw_api_repo_detail.txt
+
+# Examples for DOCS-Repositories
+curl https://api.github.com/repos/CMSgov/HealthCare.gov-Styleguide >> raw_api_repo_detail.txt
+curl https://api.github.com/repos/github/maturity-model >> raw_api_repo_detail.txt
+curl https://api.github.com/repos/raspberrypi/documentation >> raw_api_repo_detail.txt
+curl https://api.github.com/repos/bundestag/gesetze >> raw_api_repo_detail.txt
+curl https://api.github.com/repos/fsr-itse/docs >> raw_api_repo_detail.txt
+
+# Examples for WEB-Repositories
+curl https://api.github.com/repos/BloombergMedia/whatiscode >> raw_api_repo_detail.txt
+curl https://api.github.com/repos/JaceRobinson8/jacerobinson8.github.io >> raw_api_repo_detail.txt
+curl https://api.github.com/repos/rubymonstas-zurich/rubymonstas-zurich.github.io >> raw_api_repo_detail.txt
+curl https://api.github.com/repos/ianli/elbowpatched-boilerplate >> raw_api_repo_detail.txt
+curl https://api.github.com/repos/whoisjuan/whoisjuan.github.io >> raw_api_repo_detail.txt
+
+# Examples for DATA-Repositories
+curl https://api.github.com/repos/ericfischer/housing-inventory >> raw_api_repo_detail.txt
+curl https://api.github.com/repos/GSA/data >> raw_api_repo_detail.txt
+curl https://api.github.com/repos/OpenExoplanetCatalogue/open_exoplanet_catalogue >> raw_api_repo_detail.txt
+curl https://api.github.com/repos/Hipo/university-domains-list >> raw_api_repo_detail.txt
+curl https://api.github.com/repos/minimaxir/big-list-of-naughty-strings >> raw_api_repo_detail.txt
