0b6f3d28e94da95f7ceb53c7a62df23b96ab8bfe
Carsten Walther <carsten.walther@student.hpi.de>
2016-10-30 14:43:53 +0100

    add repo list

--- dataAggregation/fetching.py
+++ dataAggregation/fetching.py
@@ -1,8 +1,11 @@
 
 from github import Github
 
-githubClient = Github()
-print(dir(githubClient))
-for repo in githubClient.get_repos():
-	print(repo.name)
-	break
+API_TOKEN = '0b8801a77eaea265f203f0a4b13d3d22739a6451'
+
+githubClient = Github(API_TOKEN)
+
+with open('../data/repoURLs.txt', 'w') as f:
+	#i = 0
+	for repo in githubClient.get_repos():
+		f.write(repo.html_url + '\n')
