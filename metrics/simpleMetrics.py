796960fee598b9f2cd8ea2ab2ca9019a852ce8b7
Carsten Walther <carsten.walther@student.hpi.de>
2016-10-31 16:31:33 +0100

    add watcher metric

--- metrics/simpleMetrics.py
+++ metrics/simpleMetrics.py
@@ -6,6 +6,13 @@ def getRepoSize(repoURL):
 	repo = githubClient.get_repo(getFullName(repoURL))
 	return repo.size
 
+@CachedMetric
+def getWatcherCount(repoURL):
+	print('This (watcher) is actually executed')
+	repo = githubClient.get_repo(getFullName(repoURL))
+	return repo.watchers
+
 if __name__ == '__main__':
 	repoURL = 'https://github.com/marfarma/handsoap'
 	print('Repo size:', getRepoSize(repoURL))
+	#print('Repo watchers:', getWatcherCount(repoURL))
