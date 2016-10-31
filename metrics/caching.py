4a5daee87a4ea7af70fd82590a3b40d04988a372
Carsten Walther <carsten.walther@student.hpi.de>
2016-10-31 16:26:51 +0100

    implement CachedMetric decorator

---
+++ metrics/caching.py
@@ -0,0 +1,55 @@
+import os
+import json
+from github import Github
+
+API_TOKEN = '0b8801a77eaea265f203f0a4b13d3d22739a6451'
+githubClient = Github(API_TOKEN)
+
+class CachedMetric(object):
+
+	def __init__(self, f):
+		# TODO: add function to list here
+		self.function = f
+
+	def __call__(self, *args, **kwargs):
+		result = cacheLookup(self.function.__name__, args[0])
+		if result == None:
+			result = self.function(*args, **kwargs)
+			writeToCache(self.function.__name__, result, args[0])
+		return result
+
+def getFilename(repoURL):
+	fullname = getFullName(repoURL)
+	# Problem: getId is an API call
+	# save repo metrics to be able to load it with input URL
+	return '../data/repoMetrics/' + str(getId(fullname)) + '.json'	
+
+def cacheLookup(metricName, repoURL):
+	try:
+		with open(getFilename(repoURL), 'r') as data_file:
+			data = json.load(data_file)
+			return data[metricName]
+	except (KeyError, FileNotFoundError):
+		return None
+
+def writeToCache(metricName, result, repoURL):
+	filename = getFilename(repoURL)
+	data = {}
+	try:
+		with open(filename, 'r') as data_file:
+			data = json.load(data_file)
+	except FileNotFoundError:
+		# use empty directory
+		pass
+	data[metricName] = result
+	with open(filename, 'w') as data_file:
+			json.dump(data, data_file)
+
+
+def getFullName(repoURL):
+	tokens = repoURL.split('/')
+	return tokens[-2] + '/' + tokens[-1]
+
+def getId(fullname):
+	repo = githubClient.get_repo(fullname)
+	return repo.id
--- metrics/simpleMetrics.py
+++ metrics/simpleMetrics.py
@@ -1,40 +1,10 @@
-import os
-import json
-from github import Github
+from caching import CachedMetric, getFullName, githubClient
 
-
-API_TOKEN = '0b8801a77eaea265f203f0a4b13d3d22739a6451'
-githubClient = Github(API_TOKEN)
-
-def getRepoSize(repoURL) -> 'metric':
-	fullname = getFullName(repoURL)
-	filename = '../data/repoMetrics/' + str(getId(fullname)) + '.json'
-	key = 'repoSize'
-	data = None
-	if os.path.isfile(filename):
-		with open(filename) as data_file:
-			data = json.load(data_file)
-		if key not in data:
-			data[key] = fetchRepoSize(fullname)
-	else:
-		data = {key: fetchRepoSize(fullname)}
-
-	with open(filename, 'w') as outfile:
-		json.dump(data, outfile)
-
-	return data[key]
-
-def fetchRepoSize(repoFullName):
-	repo = githubClient.get_repo(repoFullName)
+@CachedMetric
+def getRepoSize(repoURL):
+	print('This is actually executed')
+	repo = githubClient.get_repo(getFullName(repoURL))
 	return repo.size
-
-def getFullName(repoURL):
-	tokens = repoURL.split('/')
-	return tokens[-2] + '/' + tokens[-1]
-
-def getId(fullname):
-	repo = githubClient.get_repo(fullname)
-	return repo.id
 
 if __name__ == '__main__':
 	repoURL = 'https://github.com/marfarma/handsoap'
