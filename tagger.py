39d2ccb2a2e1aa4406cacdded375c69f86085b57
Carsten Walther <carsten.walther@student.hpi.de>
2016-12-22 18:18:55 +0100

    improve tagger

--- tagger.py
+++ tagger.py
@@ -1,5 +1,6 @@
 import random
 import platform
+import sys
 from subprocess import call
 
 import github
@@ -21,51 +22,56 @@ DATA: Repositories für die Speicherung von Datensätzen
 OTHER: Repositories, die sich nicht in die anderen Kategorien einordnen lassen"""
 
 githubClient = github.Github(API_TOKEN)
-filename = 'data/testset.csv'
+FILENAME = 'data/testset.csv'
 MAX_REPO_ID = 76000000
+SEPARATOR = ','
 
 if platform.system() == 'Darwin':
     command = 'open'
 elif platform.system() == 'Linux':
     command = 'xdg-open'
 else:
-    # throw error
-    pass
+    raise NotImplementedError("Not implemented for Windows")
 
 
 def read_visited_repos():
+    visited_repos = set()
     try:
-        with open(filename, 'r') as file:
+        with open(FILENAME, 'r') as file:
             for line in file:
-                print(line)
+                visited_repos.add(line.split(SEPARATOR)[0])
     except FileNotFoundError:
+        pass
+    finally:
         return set()
 
 
 def tag_repo(url):
-    tags = set(["DEV", "HW", "EDU", "DOCS", "WEB", "DATA", "OTHER"])
+    tags = {"DEV", "HW", "EDU", "DOCS", "WEB", "DATA", "OTHER"}
     call([command, url])
     print(explanation)
     user_input = input("Classify:")
     if user_input.upper() in tags:
         return user_input.upper()
+    elif user_input.upper() == 'EXIT':
+        sys.exit()
     else:
         return None
 
+if __name__ == '__main__':
+    visited_repos = read_visited_repos()
 
-visited_repos = read_visited_repos()
-
-with open(filename, 'a') as file:
-    while True:
-        try:
-            repo = githubClient.get_repo(random.randrange(MAX_REPO_ID))
-        except github.GithubException:
-            # try different id
-            continue
-        url = repo.html_url
-        if url in visited_repos:
-            continue
-        tag = tag_repo(url)
-        if tag is not None:
-            file.write(repo.html_url + ',' + tag + '\n')
-            visited_repos.add(id)
+    with open(FILENAME, 'a') as file:
+        while True:
+            try:
+                repo = githubClient.get_repo(random.randrange(MAX_REPO_ID))
+                url = repo.html_url
+            except github.GithubException:
+                # try different id
+                continue
+            if url in visited_repos:
+                continue
+            tag = tag_repo(url)
+            if tag is not None:
+                file.write(repo.html_url + SEPARATOR + tag + '\n')
+                visited_repos.add(id)
