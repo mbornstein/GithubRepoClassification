17190bc1edb4253eb812659f9968b72670c0fe0b
mbornstein <marvin.bornstein@student.hpi.uni-potsdam.de>
2017-01-13 10:40:12 +0100

    remove erbosity

--- TwoStepClassifier.py
+++ TwoStepClassifier.py
@@ -10,17 +10,17 @@ class TwoStepClassifier:
     def fit(self, data, y):
         dev_or_empty = np.array(['      ', 'DEV'])[(y == 'DEV') * 1]
         self.model1.fit(data, dev_or_empty)
-        print(self.model1.score(data, y))
+        # print(self.model1.score(data, y))
         self.model2.fit(data[y != 'DEV'], y[y != 'DEV'])
-        print(self.model2.score(data[y != 'DEV'], y[y != 'DEV']))
+        # print(self.model2.score(data[y != 'DEV'], y[y != 'DEV']))
 
     def predict(self, data):
         y_pred1 = self.model1.predict(data)
-        print(y_pred1)
+        # print(y_pred1)
         y_pred2 = self.model2.predict(data)
-        print(y_pred2)
+        # print(y_pred2)
         y_pred1[y_pred1 != 'DEV'] = y_pred2[y_pred1 != 'DEV']
-        print(y_pred1)
+        # print(y_pred1)
         return y_pred1
 
     def score(self, data, y):
